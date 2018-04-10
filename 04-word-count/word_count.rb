668f5f37402a6f8c3de557c4c4b108ef62f6c798
emilcecarlisa <emilcecarlisa@gmail.com>
2018-04-09 20:17:10 -0700

    Corrected function to pass tests

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -3,27 +3,34 @@
 # establish what a word is
 def words(input)
   expectedCounts = {}
-  # input = input.split('')
 
-  puts "#{input}"
+  words = input.split(' ')
 
-  index = 0
-  left = 0
-
-  while index < input.length - 1
-    index += 1
+  words.each do |word|
+    if expectedCounts[word].nil?
+      expectedCounts[word] = 1
+    else
+      expectedCounts[word] += 1
+    end
   end
-
-  right = index
-
-  puts left
-  puts right
-
-  word = input[left..right]
-
-  expectedCounts[word] = 1
 
   return expectedCounts
 end
 
-# if the word matches a key in the array, then increment count
+
+
+# index = 0
+# left = 0
+#
+# while index < input.length - 1
+#   index += 1
+# end
+#
+# right = index
+#
+# puts left
+# puts right
+#
+# word = input[left..right]
+#
+# expectedCounts[word] = 1
